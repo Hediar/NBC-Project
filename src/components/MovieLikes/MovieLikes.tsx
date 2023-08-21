42c4b33476ffef6bc8a886b5209279b754ea8df2
Seryeong Lee <srlimvp@gmail.com>
2023-08-21 18:00:00 +0900

    feat: 좋아요 개수 표기 TrendMovies.tsx -> 영화 출력 및 좋아요를 위한 props components/MovieLikes.tsx -> 좋아요 여부 확인 및 좋아요 개수 표기

--- src/components/MovieLikes/MovieLikes.tsx
+++ src/components/MovieLikes/MovieLikes.tsx
@@ -1,34 +1,56 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import supabase from '@/supabase/config';
-import LikeButton from '../Buttons/LikeButton';
+import { create } from 'zustand';
 
 export const revalidate = 0;
 
 /**영화id props로 내려받아서, 해당 영화 id가 존재할 때
- * 현재 유저의 좋아요가 있는지 확인하고
- * 있으면 하트, 없으면 빈하트(디자인에 맞춰서) -> 전역상태로 관리 default 0
+ * 현재 유저의 좋아요가 있는지 확인하고 -> 현재 user는 zustand로 관리
+ * 있으면 하트, 없으면 빈하트(디자인에 맞춰서) -> useState로 관리
  * 좋아요 userid의 배열에 따라 개수 count
  *
  */
-const MovieLikes = (props: any) => {
-  const likeDataFetch = async () => {
-    let test = await supabase.from('reviews').select('*');
-    let { data } = await supabase.from('movielikes').select('*');
 
-    console.log('data', data);
+const useStore = create((set) => ({}));
+
+const MovieLikes = (props: any) => {
+  const [likeusercnt, setLikeusercnt] = useState(0); // 영화를 좋아하는 유저 수
+  const [likecurrentuser, setLikecurrentuser] = useState(false); // 현재 유저가 좋아하는지 여부
+
+  // const likeDataFetch = async () => {
+  //   let test = await supabase.from('reviews').select('*');
+  //   let { data } = await supabase.from('movielikes').select('*');
+
+  //   console.log('data', data);
+  // };
+
+  const currentLikeUserStateCheck = async () => {
+    const { data: currentMovieLikeData } = await supabase.from('movielikes').select('*').eq('movieid', props.movieid);
+    if (currentMovieLikeData?.length !== 0) {
+      const likeusers = currentMovieLikeData![0].userid.length;
+      setLikeusercnt(likeusers);
+      // console.log(currentMovieLikeData);
+    }
+  };
+
+  const movielikeHandler = () => {
+    let inputData = {};
   };
 
   useEffect(() => {
     // likeDataFetch();
+    currentLikeUserStateCheck();
   }, []);
 
   return (
     <div>
       MovieLikes
       <br />
-      {props.movieid} 좋아요 개수
-      <LikeButton />
+      {props.movieid}
+      좋아요 개수: {likeusercnt}
+      <br />
+      <button>좋아요</button>
     </div>
   );
 };
