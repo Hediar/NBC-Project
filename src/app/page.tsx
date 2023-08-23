f807b8d31934312ebcd1681d82330f9c3bcfbbef
Seryeong Lee <srlimvp@gmail.com>
2023-08-23 12:08:29 +0900

    fix: 헤더가 2개 나오는 현상 수정 page.tsx : 메인페이지에서 Header 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Header from '@/components/Header/Header';
 import MainPageMovies from '@/components/MainPageMovies/MainPageMovies';
 
 export default async function Home() {
   return (
     <>
       <main>
-        <Header />
         <MainPageMovies />
       </main>
     </>
