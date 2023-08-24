d1ce8a695352b58bc9da5a7f3cc29cfbabe0baf4
Seryeong Lee <srlimvp@gmail.com>
2023-08-24 19:40:12 +0900

    docs: 주석 수정

--- src/components/MovieLikes/MovieLikes.tsx
+++ src/components/MovieLikes/MovieLikes.tsx
@@ -5,13 +5,6 @@ import useUserInfoStore from '@/app/(store)/saveCurrentUserData';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { throttle } from 'lodash';
 
-/**영화id props로 내려받아서, 해당 영화 id가 존재할 때
- * 현재 유저의 좋아요가 있는지 확인하고 -> 현재 user는 zustand로 관리
- * 있으면 하트, 없으면 빈하트(디자인에 맞춰서) -> useState로 관리
- * 좋아요 userid의 배열에 따라 개수 count
- *
- */
-
 const MovieLikes = (props: { movieid: number }) => {
   const [likecurrentuser, setLikecurrentuser] = useState(false); // 현재 유저가 좋아하는지 여부
   const queryClient = useQueryClient();
@@ -54,6 +47,7 @@ const MovieLikes = (props: { movieid: number }) => {
     }
   );
 
+  // 좋아요 버튼
   const likeButtonHandler = throttle(
     async () => {
       if (userInfo.id) {
