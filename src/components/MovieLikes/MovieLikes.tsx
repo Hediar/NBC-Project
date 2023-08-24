e175efb7c7c5e43301da8a19ac7ce16d8e7bc443
Seryeong Lee <srlimvp@gmail.com>
2023-08-24 17:48:55 +0900

    feat: 좋아요 실시간 MovieLikes 폴더 MovieLikes.tsx 에서 tanksquery 적용 좋아요 버튼 클릭 시 로그인 상태에 따라 동작 or alert창 보여줌

--- src/components/MovieLikes/MovieLikes.tsx
+++ src/components/MovieLikes/MovieLikes.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import supabase from '@/supabase/config';
 import useUserInfoStore from '@/app/(store)/saveCurrentUserData';
-import { MovieLikesTable } from '@/types/types';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 /**영화id props로 내려받아서, 해당 영화 id가 존재할 때
  * 현재 유저의 좋아요가 있는지 확인하고 -> 현재 user는 zustand로 관리
@@ -12,25 +12,46 @@ import { MovieLikesTable } from '@/types/types';
  */
 
 const MovieLikes = (props: { movieid: number }) => {
-  const [likeusercnt, setLikeusercnt] = useState(0); // 영화를 좋아하는 유저 수
   const [likecurrentuser, setLikecurrentuser] = useState(false); // 현재 유저가 좋아하는지 여부
+  const queryClient = useQueryClient();
 
-  const testUser = {
-    id: 'test123'
-  };
+  const { data: currentMovieLikeData } = useQuery<any>({
+    queryKey: ['movieLikes', props.movieid],
+    queryFn: async () => {
+      const response = await supabase.from('movielikes').select('*').eq('movieid', props.movieid);
+      return response.data;
+    },
+    enabled: Boolean(props.movieid) // movieid가 있을 때만 요청
+  });
 
   const { userInfo } = useUserInfoStore();
 
-  const currentLikeUserStateCheck = async () => {
-    const { data: currentMovieLikeData } = await supabase.from('movielikes').select('*').eq('movieid', props.movieid);
+  const mutation = useMutation(
+    async () => {
+      const { data: likesTable } = await supabase.from('movielikes').select('*').eq('movieid', props.movieid);
 
-    if (currentMovieLikeData?.length !== 0) {
-      const likeusers = currentMovieLikeData![0].user_id.length;
-      setLikeusercnt(likeusers);
+      if (likesTable?.length) {
+        if (likesTable[0]?.user_id.includes(userInfo.id)) {
+          const { data: users } = await supabase.from('movielikes').select('user_id').eq('movieid', props.movieid);
+          const newUsers = users![0].user_id.filter((id: string) => id !== userInfo.id);
+          await supabase.from('movielikes').update({ user_id: newUsers }).eq('movieid', props.movieid);
+        } else {
+          const { data: users } = await supabase.from('movielikes').select('user_id').eq('movieid', props.movieid);
+          const newUsers = [...users![0].user_id, userInfo.id];
+          await supabase.from('movielikes').update({ user_id: newUsers }).eq('movieid', props.movieid);
+        }
+      } else {
+        const newUsers = { movieid: props.movieid, user_id: [userInfo.id] };
+        await supabase.from('movielikes').insert(newUsers);
+      }
+    },
+    {
+      // 성공 시에 캐시 업데이트
+      onSuccess: () => {
+        queryClient.invalidateQueries(['movieLikes', props.movieid]);
+      }
     }
-  };
-
-  const checklogin = () => {};
+  );
 
   const likeButtonHandler = async () => {
     /**
@@ -43,50 +64,43 @@ const MovieLikes = (props: { movieid: number }) => {
      * 영화 id가 db에 없을경우?
      * 새로운 row 추가
      */
+    userInfo.id ? mutation.mutate() : alert('로그인 해주세요!');
+
+    /** 
     const { data: likesTable } = await supabase.from('movielikes').select('*').eq('movieid', props.movieid);
 
-    console.log('table', likesTable);
-
     if (likesTable?.length) {
-      if (likesTable[0]?.user_id.includes(testUser.id)) {
+      if (likesTable[0]?.user_id.includes(userInfo.id)) {
         const { data: users } = await supabase.from('movielikes').select('user_id').eq('movieid', props.movieid);
-        const newUsers = [...users![0].user_id.filter((id: string) => id !== testUser.id)];
+        const newUsers = [...users![0].user_id.filter((id: string) => id !== userInfo.id)];
         console.log('좋아요 취소', newUsers);
         const { error } = await supabase.from('movielikes').update({ user_id: newUsers }).eq('movieid', props.movieid);
 
         console.log('영화 row가 있고 현재 userid가 있을 때 ', error);
-        return;
       } else {
         const { data: users } = await supabase.from('movielikes').select('user_id').eq('movieid', props.movieid);
-        const newUsers = [...users![0].user_id, testUser.id];
+        const newUsers = [...users![0].user_id, userInfo.id];
         console.log(newUsers);
         const { error } = await supabase.from('movielikes').update({ user_id: newUsers }).eq('movieid', props.movieid);
         console.log('영화 row가 있고 현재 userid가 없을 때 ');
-        return;
       }
     } else {
       // 영화 db에 열이 없을 경우
-      const newUsers: MovieLikesTable = { movieid: props.movieid, user_id: [`${testUser.id}`] };
+      const newUsers = { movieid: props.movieid, user_id: [`${userInfo.id}`] };
       console.log('test', newUsers);
 
       const { error } = await supabase.from('movielikes').insert(newUsers);
       console.log('영화 row가 없을때');
-      return;
     }
+    */
   };
-
-  useEffect(() => {
-    currentLikeUserStateCheck();
-
-    // console.log(userInfo);
-  }, []);
 
   return (
     <div>
       MovieLikes
       <br />
       {props.movieid}
-      좋아요 개수: {likeusercnt}
+      좋아요 개수: {currentMovieLikeData?.length ? currentMovieLikeData[0].user_id.length : 0}
       <br />
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
