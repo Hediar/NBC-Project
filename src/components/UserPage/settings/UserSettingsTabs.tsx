7261a2686a278855e03060be1f2aeb591c9cc5b2
Kim Hwanhoon <kimhwanhoon@outlook.com>
2023-08-28 14:14:07 +0900

    type 추가

---
+++ src/components/UserPage/settings/UserSettingsTabs.tsx
@@ -0,0 +1,11 @@
+const UserSettingsTabs = () => {
+  return (
+    <div className="w-1/4">
+      <div>이메일 변경</div>
+      <div>닉네임 변경</div>
+      <div>비밀번호 변경</div>
+    </div>
+  );
+};
+
+export default UserSettingsTabs;
