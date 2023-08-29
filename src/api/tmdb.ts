c1d4157dd596c40ee2e9f92f7057236371a7bb56
Lee Jiyoung <lizzie.yyy@gmail.com>
2023-08-30 03:23:17 +0900

    feat: 영화선택 페이지네이션

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -121,9 +121,9 @@ const getCreditsData = async (id: string) => {
   return { appearences: creditsData.cast, productions: creditsData.crew };
 };
 
-export const searchReviewMovies = async (query: string) => {
+export const searchReviewMovies = async (query: string, currentPage: number = 1) => {
   const searchRes = await fetch(
-    `${process.env.NEXT_PUBLIC_TMDB_BASE_URL}search/movie?query=${query}&include_adult=true&language=ko-KR&page=1`,
+    `${process.env.NEXT_PUBLIC_TMDB_BASE_URL}search/movie?query=${query}&include_adult=true&language=ko-KR&page=${currentPage}`,
     options
   );
   const searchData = await searchRes.json();
