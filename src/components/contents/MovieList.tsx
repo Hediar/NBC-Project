bfc2c290c6777aafe98c7f63004f67e39fd60708
Seryeong Lee <srlimvp@gmail.com>
2023-08-31 17:47:05 +0900

    feat: 최신순으로 불러오기, 더보기로 데이터 가져오기  다른 필터링 적용 시 초기화 안됨, 검색 input창 크기가 자꾸 변경됨  검색에 따라 다른 url 및 데이터 적용되도록 수정 필요

--- src/components/contents/MovieList.tsx
+++ src/components/contents/MovieList.tsx
@@ -14,75 +14,56 @@ const MovieList = () => {
   const [searchMovieValue, setSearchMovieValue] = useState('');
   const [filteredData, setFilterefData] = useState<any[]>([]);
 
-  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태 추가
-  const [dataList, setDataList] = useState<any>(); // 영화 목록을 상태로 관리
-  console.log(sortingOption, searchMovieValue);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [dataList, setDataList] = useState<any>([]);
 
   const today = dayjs();
   const formattedCurrentDate = today.format('YYYY-MM-DD');
 
   const fetchMovieData = async (page: number) => {
+    // fetchMovieData 함수 수정
     if (searchMovieValue) {
-      console.log('검색');
       const data = await contentPageGetDataSearch(searchMovieValue, searchType, page);
-
       setDataList(data);
+      const results = data.results;
+      setFilterefData([...filteredData, ...results]);
     } else {
-      console.log('검색x', sortingOption, formattedCurrentDate, page);
       const data = await contentPageGetDataDiscover(sortingOption, formattedCurrentDate, page);
-
       setDataList(data);
-    }
-    if (page === 1) {
-      console.log('test', dataList);
-      setFilterefData(dataList!.results); // 첫 페이지일 경우 데이터를 설정
-    } else if (page !== 1 && page < dataList!.total_pages) {
-      setFilterefData((prevMovies) => [...prevMovies, ...dataList!.results]); // 다음 페이지일 경우 기존 데이터에 추가
+      const results = data.results;
+      setFilterefData([...filteredData, ...results]);
     }
   };
 
   const fetchMore = () => {
     if (currentPage < dataList!.total_pages) {
       setCurrentPage((prevPage) => prevPage + 1);
-    } // 다음 페이지 로드
-  };
-
-  const handleSortingChange = (value: string) => {
-    setSortingOption(value);
-    setCurrentPage(1); // 정렬 옵션 변경 시 첫 페이지로 초기화
+    }
   };
 
   useEffect(() => {
     fetchMovieData(currentPage);
-  }, [currentPage]);
+  }, [currentPage, sortingOption, searchMovieValue, searchType]); // 검색 타입이나 검색어가 바뀔 때도 데이터를 가져옴
 
   useEffect(() => {
-    handleSortingChange(sortingOption);
-  }, [sortingOption]);
+    setCurrentPage(1); // 정렬 옵션이 변경될 때 첫 페이지로 초기화
+  }, [sortingOption, searchMovieValue, searchType]);
 
   let contents;
 
   if (!dataList) {
     contents = <>검색 결과가 없습니다.</>;
   } else {
-    contents = filteredData.map((movie: MovieData) => {
-      return (
-        <Link href={'/detail/' + movie.id} key={movie.id} className="w-56 h-full flex flex-col gap-2 items-center">
-          <img
-            className="rounded-xl h-2/3 w-10/12"
-            alt="poster"
-            src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
-          />
-          {movie.id}
-          <div className="flex flex-col gap-1 w-full h-1/3">
-            <h4 className="text-sm font-bold">{movie.title}</h4>
-            <div className="flex gap-2">
-              <p className="text-xs">{movie.release_date}</p>
-            </div>
-          </div>
-        </Link>
-      );
-    });
+    contents = (
+      <div className="grid grid-cols-4 gap-4">
+        {filteredData.map((movie: MovieData) => (
+          <Link href={`/detail/${movie.id}`} key={movie.id}>
+            <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
+            <h3>{movie.title}</h3>
+          </Link>
+        ))}
+      </div>
+    );
   }
 
   return (
@@ -96,10 +77,12 @@ const MovieList = () => {
           setSearchType={setSearchType}
         />
       </div>
-      <div>{contents}</div>
-      <div onClick={fetchMore} className="bg-blue-700">
-        더보기
-      </div>
+      <div className="p-5">{contents}</div>
+      {currentPage < dataList?.total_pages && ( // 다음 페이지가 있는 경우에만 더보기 버튼 표시
+        <div onClick={fetchMore} className="bg-blue-700 cursor-pointer text-center py-2 text-white">
+          더보기
+        </div>
+      )}
     </div>
   );
 };
