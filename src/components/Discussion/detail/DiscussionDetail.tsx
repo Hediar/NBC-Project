7a9557dc98581c4e1e420896162c1d55c9d1c78f
CTDKSKM <skmdldy001@gmail.com>
2023-09-01 10:52:57 +0900

    토론상세 다음목록 이전목록 가져오기 구현
    supabase lt gt 메서드 사용

--- src/components/Discussion/detail/DiscussionDetail.tsx
+++ src/components/Discussion/detail/DiscussionDetail.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import OptionVote from './OptionVote';
 import DiscussionCommentContainer from './comment/DiscussionCommentContainer';
 import EditDeleteBox from './EditDeleteBox';
-import { getDiscussionPostDetail, getDiscussionPostOption, getRelatedDiscussionPost } from '@/api/supabase-discussion';
+import {
+  getDiscussionPostDetail,
+  getDiscussionPostOption,
+  getNextDiscussionPost,
+  getPrevDiscussionPost,
+  getRelatedDiscussionPost
+} from '@/api/supabase-discussion';
 import DiscussionContent from './DiscussionContent';
 import RelatedDiscussionPost from './related-discussion/RelatedDiscussionPost';
 import Link from 'next/link';
@@ -12,9 +18,10 @@ interface Props {
 }
 
 const DiscussionDetail = async ({ discussionId }: Props) => {
-  //discussionId 조회해서 db에서 내용 가져오기.
   const postData = await getDiscussionPostDetail(+discussionId);
   const relatedData = await getRelatedDiscussionPost({ genreIds: postData.movie_genreIds, movieId: postData.movie_id });
+  const prevPostData = await getPrevDiscussionPost({ postId: postData.post_id, movieId: postData.movie_id });
+  const nextPostData = await getNextDiscussionPost({ postId: postData.post_id, movieId: postData.movie_id });
 
   return (
     <div className="">
@@ -22,19 +29,32 @@ const DiscussionDetail = async ({ discussionId }: Props) => {
 
       <div className="flex justify-between w-4/5 mx-auto">
         <div className="w-2/3">
-          <section className="min-h-[60vh] flex flex-col items-center">
+          <section className="min-h-[40vh] flex flex-col items-center relative">
             <div className="flex flex-col justify-center mx-auto">
-              <h3 className="text-3xl font-bold m-5">
-                {postData?.title}
-                {postData?.movie_genreIds[0]}
-              </h3>
+              <h3 className="text-3xl font-bold m-5">{postData?.title}</h3>
               <p className="text-xl">{postData?.content}</p>
             </div>
 
             <OptionVote postId={postData.post_id} voteCount={postData.vote_count} />
+
+            {prevPostData?.length ? (
+              <Link
+                href={`/discussion/detail/${prevPostData[0].post_id}`}
+                className="absolute top-1/2 -translate-y-2/4 left-0"
+              >
+                {'<'}
+              </Link>
+            ) : null}
+            {nextPostData?.length ? (
+              <Link
+                href={`/discussion/detail/${nextPostData[0].post_id}`}
+                className="absolute top-1/2 -translate-y-2/4 right-0"
+              >
+                {'>'}
+              </Link>
+            ) : null}
           </section>
 
-          {/* 작성자랑 유저id랑 같을때만 기능하도록 */}
           <div>
             <EditDeleteBox postId={postData.post_id} authorId={postData.user_id} />
           </div>
@@ -47,7 +67,9 @@ const DiscussionDetail = async ({ discussionId }: Props) => {
         <div className="w-[20vw]">
           <p>관련된 다른 토픽</p>
           {relatedData?.length ? (
-            relatedData?.map(async (relatedDiscussionData) => {
+            relatedData?.map(async (relatedDiscussionData, idx) => {
+              if (idx > 3) return null;
+
               const relatedOption = await getDiscussionPostOption(relatedDiscussionData.post_id);
 
               return (
