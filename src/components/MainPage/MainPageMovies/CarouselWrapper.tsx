165fde1eb2e786060d69f3cd1a1761de0e23eb1b
Seryeong Lee <srlimvp@gmail.com>
2023-09-04 09:38:16 +0900

    중간저장

---
+++ src/components/MainPage/MainPageMovies/CarouselWrapper.tsx
@@ -0,0 +1,57 @@
+'use client';
+import React, { useState, useEffect, useCallback } from 'react';
+import useEmblaCarousel, { EmblaCarouselType, EmblaOptionsType } from 'embla-carousel-react';
+import { NextButton, PrevButton } from '@/components/common/Slider/ArrowsDotsButtons';
+
+type PropType = {
+  slides: number[];
+  options?: EmblaOptionsType;
+};
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+
+  const scrollPrev = useCallback(() => emblaApi && emblaApi.scrollPrev(), [emblaApi]);
+  const scrollNext = useCallback(() => emblaApi && emblaApi.scrollNext(), [emblaApi]);
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setPrevBtnDisabled(!emblaApi.canScrollPrev());
+    setNextBtnDisabled(!emblaApi.canScrollNext());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+    onSelect(emblaApi);
+    emblaApi.on('reInit', onSelect);
+    emblaApi.on('select', onSelect);
+  }, [emblaApi, onSelect]);
+
+  return (
+    <>
+      <div className="embla">
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
+            {slides.map((index) => (
+              <div className="embla__slide" key={index}>
+                <div className="embla__slide__number">
+                  <span>{index + 1}</span>
+                </div>
+                <img className="embla__slide__img" src={} alt="Your alt text" />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="embla__buttons">
+          <PrevButton onClick={scrollPrev} disabled={prevBtnDisabled} />
+          <NextButton onClick={scrollNext} disabled={nextBtnDisabled} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CarouselWrapper;
--- src/components/common/ScrollToTopButton.tsx
+++ src/components/common/ScrollToTopButton.tsx
@@ -6,7 +6,7 @@ const ScrollToTopButton = () => {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    // 스크롤 이벤트를 추가하여 버튼 표시 여부를 업데이트합니다.
+    // 스크롤 이벤트를 추가하여 버튼 표시 여부를 업데이트
     const handleScroll = () => {
       if (window.pageYOffset > 300) {
         setIsVisible(true);
@@ -15,10 +15,10 @@ const ScrollToTopButton = () => {
       }
     };
 
-    // 스크롤 이벤트 리스너를 등록합니다.
+    // 스크롤 이벤트 리스너를 등록
     window.addEventListener('scroll', handleScroll);
 
-    // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거합니다.
+    // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
