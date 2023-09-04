31ee4146ac79b5f38e2dfd80f08769aed6244caf
CTDKSKM <skmdldy001@gmail.com>
2023-09-04 12:31:01 +0900

    빌드에러 수정
    정적타입이라 metadata로 되는부분이었는데 generateMetadata():Promise<Metadata> 로
    리턴타입 지정해줘가지고  빌드시 에러났던부분 수정

--- src/app/discussion/regist/layout.tsx
+++ src/app/discussion/regist/layout.tsx
@@ -1,10 +1,8 @@
-import { Metadata, ResolvingMetadata } from 'next';
+import { Metadata } from 'next';
 
-export async function generateMetadata(parent: ResolvingMetadata): Promise<Metadata> {
-  return {
-    title: `토론작성 - 무비바바`
-  };
-}
+export const metadata: Metadata = {
+  title: '토론작성 - 무비바바'
+};
 
 export default async function MovieDetailLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
