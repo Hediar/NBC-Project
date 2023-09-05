38ba67c665b50627c41646f1178cd26cdb76bb3f
Seryeong Lee <srlimvp@gmail.com>
2023-09-06 00:06:48 +0900

    fix Icons error

--- src/styles/icons/Icons24.tsx
+++ src/styles/icons/Icons24.tsx
@@ -19,7 +19,6 @@ export const HeartLine = (props: SVGProps<SVGSVGElement>) => (
     />
   </svg>
 );
-
 export const HeartFilledColor = (props: SVGProps<SVGSVGElement>) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path
@@ -27,6 +26,9 @@ export const HeartFilledColor = (props: SVGProps<SVGSVGElement>) => (
       fill="#FF1D53"
       stroke="#FF1D53"
       stroke-width="2"
+    />
+  </svg>
+);
 
 export const StarFill = (props: SVGProps<SVGSVGElement>) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
