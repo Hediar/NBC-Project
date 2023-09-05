22bd6ffba659c3c77dd169749c12068d08e36b5f
Seryeong Lee <srlimvp@gmail.com>
2023-09-06 04:44:58 +0900

    style: 리뷰 컬러 입힘

--- src/components/MainPage/LatestReviews.tsx
+++ src/components/MainPage/LatestReviews.tsx
@@ -5,14 +5,15 @@ export const revalidate = 0;
 
 const LatestReviews = async () => {
   const latestReviewData = await getLatestReviews();
+  const backgroundColors = ['MainYellow2', 'MainBlue2', 'MainPurple2', 'MainOrange2'];
   return (
     <div className="p-5 flex-1">
       <h1 className="h1_suit">최신 리뷰</h1>
-      <div className="">
-        {latestReviewData?.map((review) => {
+      <div>
+        {latestReviewData?.map((review, index) => {
           return (
-            <Link key={review.reviewid} href={`/review/${review.reviewid}`} className="w-full ">
-              <div className=" p-4 border border-gray-300 rounded h-23">
+            <Link key={review.reviewid} href={`/review/${review.reviewid}`} className="w-full mb-16">
+              <div className={`p-4 border border-gray-300 rounded h-36 bg-${backgroundColors[index]}`}>
                 <div className="font-bold mb-2">{review.username}</div>
                 <div className="overflow-ellipsis">{review.content}</div>
               </div>
