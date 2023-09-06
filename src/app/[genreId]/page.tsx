93cd04e35a11614f7668fa4d79a89f2162dd0b7a
Seryeong Lee <srlimvp@gmail.com>
2023-09-06 18:54:48 +0900

    style: 인기영화 크기 조절중

--- src/app/[genreId]/page.tsx
+++ src/app/[genreId]/page.tsx
@@ -12,7 +12,7 @@ const GenreMovies = ({ params }: { params: { genreId: string } }) => {
   const { genreId } = params;
   return (
     <main>
-      <section>
+      <section className="my-8">
         <div className="flex flex-col items-end my-[120px] md:flex-row md:justify-between">
           <h2 className="h1_suit">😎장르별 인기 영화</h2>
           <Link href={'/movielist'} className="flex mt-4 mr-[190px] md:mt-0">
@@ -25,7 +25,7 @@ const GenreMovies = ({ params }: { params: { genreId: string } }) => {
         <TrendMoives genreId={genreId} />
       </section>
       <section className="mt-[180px] mb-[160px] flex flex-col md:flex-row">
-        <div className="md:w-1/2 mb-8 md:mb-0">
+        <div className="md:w-1/2 mb-8 md:mb-0 mr-[58px]">
           <LatestReviews />
         </div>
         <div className="md:w-1/2">
--- src/components/MainPage/Carousel/TrendMoviesCarousel.tsx
+++ src/components/MainPage/Carousel/TrendMoviesCarousel.tsx
@@ -40,21 +40,21 @@ const TrendMoviesCarousel: React.FC<PropType> = (props) => {
   }, [emblaApi, onSelect]);
 
   return (
-    <div className="w-full relative">
+    <div className="w-full relative h-96 md:h-[460px]">
       <div className="overflow-hidden relative h-[460px] " ref={emblaRef}>
-        <div className="flex flex-col flex-wrap h-44 flex-none ml-28">
+        <div className="flex flex-col flex-wrap h-44 flex-none ml-8 md:ml-28">
           {slides.map((slide, index) => (
-            <div className={'w-[60%] relative mx-1'} key={index}>
+            <div className={'relative mx-1 sm:w-[100%] md:w-[60%] lg:w-[60%] xl:w-[30%]'} key={index}>
               {slide}
             </div>
           ))}
         </div>
       </div>
 
-      <div className="flex justify-between absolute left-[190px] top-1/2 -translate-y-1/2 bg-transparent cursor-pointer z-10">
+      <div className="flex justify-between absolute left-[30px] top-[50%] -translate-y-1/2 bg-transparent cursor-pointer z-10">
         <PrevButton onClick={scrollPrev} disabled={prevBtnDisabled} />
       </div>
-      <div className="flex justify-between absolute right-[190px] top-1/2 -translate-y-1/2 bg-transparent cursor-pointer z-10">
+      <div className="flex justify-between absolute right-[30px] top-[50%] -translate-y-1/2 bg-transparent cursor-pointer z-10">
         <NextButton onClick={scrollNext} disabled={nextBtnDisabled} />
       </div>
     </div>
