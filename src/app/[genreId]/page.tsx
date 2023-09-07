19f663e03d5197a2b55ce8614461ecd3e3f972a0
Seryeong Lee <srlimvp@gmail.com>
2023-09-07 21:19:00 +0900

    fix: 메인 페이지 컴포넌트 하나로 통일 -> MainPage.tsx MainPage 내 컴포넌트들 폴더 정리

--- src/app/[genreId]/page.tsx
+++ src/app/[genreId]/page.tsx
@@ -1,42 +1,10 @@
-import HotTopics from '@/components/MainPage/HotTopics';
-import LatestMovies from '@/components/MainPage/LatestMovies';
-import LatestReviews from '@/components/MainPage/LatestReviews';
-import KeywordButtons from '@/components/MainPage/MainPageMovies/KeywordButtons';
-import TrendMoives from '@/components/MainPage/MainPageMovies/TrendMoives';
-import { ArrowRight2 } from '@/styles/icons/Icons24';
-import Link from 'next/link';
+import MainPage from '@/components/MainPage/MainPage';
 
 export const revalidate = 0;
 
 const GenreMovies = ({ params }: { params: { genreId: string } }) => {
   const { genreId } = params;
-  return (
-    <main>
-      <section className="my-8">
-        <div className="flex flex-col items-end md:flex-row my-[120px] justify-between">
-          <h2 className="h3_suit mx-auto md:text-6xl leading-[72px]">😎장르별 인기 영화</h2>
-          <Link href={'/movielist'} className="flex mt-4 md:mr-[190px]">
-            더보기 <ArrowRight2 />
-          </Link>
-        </div>
-        <div className="p-2 mx-5 md:mx-32">
-          <KeywordButtons params={params.genreId} />
-        </div>
-        <TrendMoives genreId={genreId} />
-      </section>
-      <section className="flex-row sm:flex">
-        <div className="sm:w-1/2 mb-8 md:mb-0 mr-[58px] lg:mb-[160px] 2xl:ml-[192px]">
-          <LatestReviews />
-        </div>
-        <div className="sm:w-1/2 2xl:mr-[192px]">
-          <LatestMovies />
-        </div>
-      </section>
-      <section className=" bg-GreyScaleWhite2">
-        <HotTopics />
-      </section>
-    </main>
-  );
+  return <MainPage params={genreId} />;
 };
 
 export default GenreMovies;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,7 @@
-import HotTopics from '@/components/MainPage/HotTopics';
-import LatestMovies from '@/components/MainPage/LatestMovies';
-import LatestReviews from '@/components/MainPage/LatestReviews';
-import KeywordButtons from '@/components/MainPage/MainPageMovies/KeywordButtons';
-import TrendMoives from '@/components/MainPage/MainPageMovies/TrendMoives';
-import { ArrowRight2 } from '@/styles/icons/Icons24';
-import Link from 'next/link';
+import MainPage from '@/components/MainPage/MainPage';
 
 export const revalidate = 0;
 
 export default async function Home() {
-  return (
-    <main className="w-full overflow-x-hidden">
-      <section className="my-8">
-        <div className="flex relative items-end md:flex-row my-[120px] justify-between">
-          <h2 className="h3_suit mx-auto md:text-6xl leading-[72px]">😎장르별 인기 영화</h2>
-          <Link href={'/movielist'} className="flex absolute right-11 mt-4 md:mr-[190px] ">
-            더보기 <ArrowRight2 />
-          </Link>
-        </div>
-        <div className="p-2 mx-5 md:mx-32">
-          <KeywordButtons params={'all'} />
-        </div>
-        <TrendMoives genreId={'all'} />
-      </section>
-      <section className="flex-row sm:flex lg:mt-[360px]">
-        <div className="sm:w-1/2 mb-8 md:mb-0 mr-[58px] lg:mb-[160px] 2xl:ml-[192px]">
-          <LatestReviews />
-        </div>
-        <div className="sm:w-1/2 2xl:mr-[192px]">
-          <LatestMovies />
-        </div>
-      </section>
-      <section className=" bg-GreyScaleWhite2">
-        <HotTopics />
-      </section>
-    </main>
-  );
+  return <MainPage params="all" />;
 }
