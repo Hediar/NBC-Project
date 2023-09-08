d025b39de1177edcef6b01533c72ac8bae2eda30
Seryeong Lee <72387948+Hediar@users.noreply.github.com>
2023-09-08 13:54:10 +0900

    style: 최신 리뷰 배경 색 수정

--- src/components/MainPage/MainContents/LatestReviews.tsx
+++ src/components/MainPage/MainContents/LatestReviews.tsx
@@ -10,17 +10,21 @@ const LatestReviews = async () => {
   const latestReviewData = await getLatestReviews();
   const backgroundColors = ['bg-MainYellow2', 'bg-MainBlue2', 'bg-MainPurple2', 'bg-MainOrange2'];
   const getColors = latestReviewData.map((data) => data.colors[8]);
-
-  const bgClasses = getColors.map((color) => {
-    const hexColor = `#${color.map((channel: number) => channel.toString(16).padStart(2, '0')).join('')}`;
-    return `bg-[${hexColor}]`;
+  const bgStyles = getColors.map((color) => {
+    const rgbColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+    return [rgbColor];
   });
+  // const bgClasses = getColors.map((color) => {
+  //   const hexColor = `#${color.map((channel: number) => channel.toString(16).padStart(2, '0')).join('')}`;
+  //   return `${hexColor}`;
+  // });
 
-  const rgbToHex = (rgb: any) => {
-    return `#${rgb.map((channel: any) => channel.toString(16).padStart(2, '0')).join('')}`;
-  };
+  // const rgbToHex = (rgb: any) => {
+  //   return `#${rgb.map((channel: any) => channel.toString(16).padStart(2, '0')).join('')}`;
+  // };
 
-  console.log('getColors', bgClasses);
+  // console.log('getColors', bgClasses);
+  // console.log(bgStyles);
   return (
     <div className="p-5">
       <div className="flex justify-between items-end">
@@ -33,7 +37,10 @@ const LatestReviews = async () => {
         {latestReviewData?.map((review, index) => {
           return (
             <Link key={review.reviewid} href={`/review/${review.reviewid}`} className="w-full mb-16 m-4">
-              <div className={`p-4 border border-gray-300 rounded-xl h-36 ${bgClasses[index]}`}>
+              <div
+                className={`p-4 border border-gray-300 rounded-xl h-36`}
+                style={{ backgroundColor: `${bgStyles[index]}` }}
+              >
                 <div className="flex justify-between">
                   <div className="flex">
                     <Image
