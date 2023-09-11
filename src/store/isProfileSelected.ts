55f8deb82b7ac15111d21eadc1a9a7ce4f9ae600
Kim Hwanhoon <kimhwanhoon@outlook.com>
2023-09-11 18:45:24 +0900

    change avatar updated

--- src/store/isProfileSelected.ts
+++ src/store/isProfileSelected.ts
@@ -1,14 +1,16 @@
 import { create } from 'zustand';
 
 interface ToggleIsProfileSelected {
-  isProfileSelected: boolean;
-  setIsProfileSelected: (toggle: boolean) => void;
+  target: number | null;
+  isSelected: boolean;
+  setIsProfileSelected: (key: number | null, isSelected: boolean) => void;
 }
 
 const useIsProfileSelected = create<ToggleIsProfileSelected>((set) => {
   return {
-    isProfileSelected: false,
-    setIsProfileSelected: (toggle: boolean) => set({ isProfileSelected: toggle })
+    target: null,
+    isSelected: false,
+    setIsProfileSelected: (key: number | null, isSelected: boolean) => set({ target: key, isSelected })
   };
 });
 
