0859ee263eab879a4a1216200d6069f045ec1b31
Seryeong Lee <72387948+Hediar@users.noreply.github.com>
2023-09-12 21:25:17 +0900

    moviedatalist flex 수정

--- src/components/contents/MovieDataList.tsx
+++ src/components/contents/MovieDataList.tsx
@@ -7,7 +7,7 @@ const MovieDataList = ({ movieData }: { movieData: MovieData[] }) => {
   }
 
   return (
-    <div className="flex flex-wrap mx-5 p-3 gap-4 items-start justify-center sm:grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-5 2xl:mx-36">
+    <div className="flex flex-wrap mx-5 p-3 gap-4 items-start justify-center xl:grid-cols-5 2xl:mx-36">
       {movieData.map((movie: MovieData) => (
         <div className="gap-4 mb-2" key={movie.id}>
           <MovieItem movie={movie} key={movie.id} />
