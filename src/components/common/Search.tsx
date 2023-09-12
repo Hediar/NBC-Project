5eeb841184735c08871bf3afc68bcaf5ddf40215
Seryeong Lee <72387948+Hediar@users.noreply.github.com>
2023-09-12 20:20:57 +0900

    movielist style 영화 검색 부분 수정

--- src/components/common/Search.tsx
+++ src/components/common/Search.tsx
@@ -70,7 +70,11 @@ const Search = ({
   return (
     <div className="w-[570px] h-[52px]">
       <form onSubmit={handleSubmit} className="flex border border-GreyScaleGrey rounded-xl px-2 py-1">
-        <select value={searchType} onChange={handleSelectChange} className="">
+        <select
+          value={searchType}
+          onChange={handleSelectChange}
+          className="border-none rounded-none outline-transparent"
+        >
           <option value="movie">영화</option>
           <option value="person">인물</option>
         </select>
@@ -83,6 +87,7 @@ const Search = ({
           value={searchInput}
           onChange={handleChange}
         />
+
         <button type="submit" className=" font-bold py-2 px-4 rounded ml-2">
           <SearchLined />
         </button>
