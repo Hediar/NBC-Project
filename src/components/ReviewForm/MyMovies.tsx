44e2b0d4ded50259a5b1209164bee40c91b0c893
lizzieFEstudy <lizzie.yyy@gmail.com>
2023-09-13 19:03:59 +0900

    feat: 영화검색팝업-나의영화리스트에 아코디언 UI 추가

--- src/components/ReviewForm/MyMovies.tsx
+++ src/components/ReviewForm/MyMovies.tsx
@@ -5,8 +5,13 @@ import useUserInfoStore from '@/store/saveCurrentUserData';
 import supabase from '@/supabase/config';
 import { useEffect, useState } from 'react';
 import MyMoviesSwiper from './MyMoviesSwiper';
+import { ArrowDown } from '@/styles/icons/Icons24';
 
-const MyMovies = () => {
+type Props = {
+  isSearchStart: boolean;
+};
+
+const MyMovies = ({ isSearchStart }: Props) => {
   const { userInfo } = useUserInfoStore();
 
   const [likesList, setLikesList] = useState<any>([]);
@@ -44,57 +49,71 @@ const MyMovies = () => {
   }, [userInfo]);
 
   return (
-    <div className="p-10 pb-5 bg-neutral-50 rounded-2xl border border-gray-200">
-      <strong className="subtitle2_suit">나의 영화 리스트</strong>
-      <ul className="overflow-hidden relative mt-4 h-[338px]">
-        <li className="inline-block">
-          <input
-            className="peer sr-only"
-            type="radio"
-            value="myList-likes"
-            name="myList"
-            id="myList-likes"
-            defaultChecked
-          />
-          <label
-            className="inline-block mr-3 px-4 py-2 bg-white rounded-3xl border border-zinc-300 text-neutral-800 text-base font-normal leading-snug cursor-pointer peer-checked:bg-neutral-800 peer-checked:text-white transition-all duration-500 ease-in-out"
-            htmlFor="myList-likes"
-          >
-            좋아요 누른 영화
-          </label>
-          {likesList.length === 0 ? (
-            <p className="h3_suit py-10 text-center absolute left-0 w-full mt-3 mx-auto transition-all duration-500 ease-in-out translate-x-40 opacity-0 invisible peer-checked:opacity-100 peer-checked:visible peer-checked:translate-x-1">
-              좋아요 누른 영화가 없습니다.
-            </p>
-          ) : (
-            <MyMoviesSwiper
-              dataList={likesList}
-              className="absolute left-0 w-full mt-3 mx-auto transition-all duration-500 ease-in-out translate-x-40 opacity-0 invisible peer-checked:opacity-100 peer-checked:visible peer-checked:translate-x-1 pb-5"
+    <details className="group px-10 py-5 bg-neutral-50 rounded-2xl border border-gray-200" open={!isSearchStart}>
+      <summary className="inline-flex gap-3 cursor-pointer transition-all group-open:pt-5">
+        <strong className="subtitle2_suit">나의 영화 리스트</strong>
+        <span className="transition group-open:rotate-180">
+          <ArrowDown />
+        </span>
+      </summary>
+      {/* </div> */}
+      <div className="group-open:animate-fadeIn">
+        <ul className="overflow-hidden relative mt-4 h-[338px]">
+          <li className="inline-block">
+            <input
+              className="peer sr-only"
+              type="radio"
+              value="myList-likes"
+              name="myList"
+              id="myList-likes"
+              defaultChecked
             />
-          )}
-        </li>
+            <label
+              className="inline-block mr-3 px-4 py-2 bg-white rounded-3xl border border-zinc-300 text-neutral-800 text-base font-normal leading-snug cursor-pointer peer-checked:bg-neutral-800 peer-checked:text-white transition-all duration-500 ease-in-out"
+              htmlFor="myList-likes"
+            >
+              좋아요 누른 영화
+            </label>
+            {likesList.length === 0 ? (
+              <p className="h3_suit py-10 text-center absolute left-0 w-full mt-3 mx-auto transition-all duration-500 ease-in-out translate-x-40 opacity-0 invisible peer-checked:opacity-100 peer-checked:visible peer-checked:translate-x-1">
+                좋아요 누른 영화가 없습니다.
+              </p>
+            ) : (
+              <MyMoviesSwiper
+                dataList={likesList}
+                className="absolute left-0 w-full mt-3 mx-auto transition-all duration-500 ease-in-out translate-x-40 opacity-0 invisible peer-checked:opacity-100 peer-checked:visible peer-checked:translate-x-1 pb-5"
+              />
+            )}
+          </li>
 
-        <li className="inline-block">
-          <input className="peer sr-only" type="radio" value="myList-watchLater" name="myList" id="myList-watchLater" />
-          <label
-            className="inline-block mr-3 px-4 py-2 bg-white rounded-3xl border border-zinc-300 text-neutral-800 text-base font-normal leading-snug cursor-pointer peer-checked:bg-neutral-800 peer-checked:text-white transition-all duration-500 ease-in-out"
-            htmlFor="myList-watchLater"
-          >
-            찜한 영화
-          </label>
-          {likesList.length === 0 ? (
-            <p className="h3_suit py-10 text-center absolute left-0 w-full mt-3 mx-auto transition-all duration-500 ease-in-out translate-x-40 opacity-0 invisible peer-checked:opacity-100 peer-checked:visible peer-checked:translate-x-1 pb-5">
-              찜한 영화가 없습니다.
-            </p>
-          ) : (
-            <MyMoviesSwiper
-              dataList={watchLaterList}
-              className="absolute left-0 w-full mt-3 mx-auto transition-all duration-500 ease-in-out translate-x-40 opacity-0 invisible peer-checked:opacity-100 peer-checked:visible peer-checked:translate-x-1"
+          <li className="inline-block">
+            <input
+              className="peer sr-only"
+              type="radio"
+              value="myList-watchLater"
+              name="myList"
+              id="myList-watchLater"
             />
-          )}
-        </li>
-      </ul>
-    </div>
+            <label
+              className="inline-block mr-3 px-4 py-2 bg-white rounded-3xl border border-zinc-300 text-neutral-800 text-base font-normal leading-snug cursor-pointer peer-checked:bg-neutral-800 peer-checked:text-white transition-all duration-500 ease-in-out"
+              htmlFor="myList-watchLater"
+            >
+              찜한 영화
+            </label>
+            {likesList.length === 0 ? (
+              <p className="h3_suit py-10 text-center absolute left-0 w-full mt-3 mx-auto transition-all duration-500 ease-in-out translate-x-40 opacity-0 invisible peer-checked:opacity-100 peer-checked:visible peer-checked:translate-x-1 pb-5">
+                찜한 영화가 없습니다.
+              </p>
+            ) : (
+              <MyMoviesSwiper
+                dataList={watchLaterList}
+                className="absolute left-0 w-full mt-3 mx-auto transition-all duration-500 ease-in-out translate-x-40 opacity-0 invisible peer-checked:opacity-100 peer-checked:visible peer-checked:translate-x-1"
+              />
+            )}
+          </li>
+        </ul>
+      </div>
+    </details>
   );
 };
 
